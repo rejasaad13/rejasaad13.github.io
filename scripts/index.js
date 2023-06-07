3fa7fb81ce1edbc6844e80603eb56846fd16f77c
rejasaad13 <85949495+rejasaad13@users.noreply.github.com>
2023-06-07 16:59:49 +0100

    sldhi

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'June 06, 2023',
-  name: 'CHOROK'
+  birthdate: 'June 08, 2023',
+  name: 'NOUSSAIR'
 };
 
 function hideEverything() {
